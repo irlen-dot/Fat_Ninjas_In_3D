0aa9c918f4b940244e4c76df327f463e8b6ac48f
Irlen Turlykhanov <irlen.turlykhanov@gmail.com>
2024-12-07 15:12:40 +0100

    feat: player

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void ProcessInput() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+    } 
+}
